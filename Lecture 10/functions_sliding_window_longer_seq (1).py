b802f4d30c48251c4896bf7b281690b500bc4851
Shritej Shrikant Chavan <shritej24c@gmail.com>
2023-11-27 03:09:58 -0600

    Create functions_sliding_window_longer_seq (1).py

---
+++ Lecture 10/functions_sliding_window_longer_seq (1).py
@@ -0,0 +1,244 @@
+
+from transformers import Trainer
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader, Dataset
+from transformers import PreTrainedModel
+from evaluate import load
+from transformers import Pipeline
+
+from torch.utils.data import DataLoader
+
+import numpy as np
+from scipy.special import softmax
+from scipy.special import logsumexp
+from transformers import Trainer
+import torch
+import torch.nn as nn
+from evaluate import load
+import gc
+
+def free_memory():
+    """
+    Attempts to free up memory by deleting variables and running Python's garbage collector.
+    """
+    gc.collect()
+    for device_id in range(torch.cuda.device_count()):
+        torch.cuda.set_device(device_id)
+        torch.cuda.empty_cache()
+    gc.collect()
+
+
+
+def collate_function(batch, tokenizer, max_length, stride):
+
+
+    text_batch = [item['text'] for item in batch]
+
+    # Check if any of the batch items has 'label', if so process labels.
+    if all('label' in item for item in batch):
+        label_batch = [item['label'] for item in batch]
+        doc_labels = torch.tensor(label_batch, dtype=torch.long)
+    else:
+        doc_labels = None
+
+
+    # Tokenize the batch of texts
+    tokenized = tokenizer(text_batch,
+                          padding='longest',
+                          truncation=True,
+                          return_tensors='pt',
+                          max_length=max_length,
+                          return_overflowing_tokens=True,
+                          stride=stride)
+
+    # Flatten the chunks if necessary using overflow_to_sample_mapping
+    overflow_to_sample_mapping = tokenized.get("overflow_to_sample_mapping", None)
+
+    result = {
+        **tokenized,
+        "overflow_to_sample_mapping": overflow_to_sample_mapping,
+    }
+
+    # Repeat labels for the chunks based on overflow_to_sample_mapping, only if labels are present
+    if doc_labels is not None and overflow_to_sample_mapping is not None:
+        labels = doc_labels[overflow_to_sample_mapping]
+        result["labels"] = labels
+        result["doc_labels"] = doc_labels
+
+    return result
+
+
+
+def stable_log_softmax(x):
+    # Subtract the max for numerical stability along the last axis (axis=-1)
+    x_max = np.max(x, axis=-1, keepdims=True)
+    # Use logsumexp for better numerical stability
+    log_softmax = x - x_max - logsumexp(x - x_max, axis=-1, keepdims=True)
+    return log_softmax
+
+
+def aggregate_predictions (logits, aggregation_method, overflow_to_sample_mapping):
+
+    probabilities = softmax(logits, axis=-1)
+    log_probabilities = stable_log_softmax(logits)
+
+    # print('log_probs', log_probabilities.shape)
+    # print('overflow_to_sample_mapping', overflow_to_sample_mapping.shape)
+
+    # Get unique documents
+
+    unique_docs, inverse_indices = np.unique(overflow_to_sample_mapping, return_inverse=True)
+    num_docs = unique_docs.size
+
+    # Create a mask for documents
+    mask = overflow_to_sample_mapping[:, None] == unique_docs[None, :]
+
+    # Initialize the aggregated_predictions variable
+    aggregated_predictions = None
+
+    if aggregation_method == "average_log_probs":
+        # Compute average probabilities using NumPy
+        avg_log_probs = (log_probabilities[:, None, :] * mask[:, :, None].astype(float)).sum(axis=0) / mask.sum(axis=0, keepdims=True).transpose((1, 0))
+        aggregated_predictions = np.argmax(avg_log_probs, axis=-1)
+        scores = np.exp(avg_log_probs.max(axis=-1))
+
+    elif aggregation_method == "average_probs":
+        # Compute average probabilities using NumPy
+        avg_probs = (probabilities[:, None, :] * mask[:, :, None].astype(float)).sum(axis=0) / mask.sum(axis=0, keepdims=True).transpose((1, 0))
+        aggregated_predictions = np.argmax(avg_probs, axis=-1)
+        scores  = avg_probs.max(axis=-1)
+
+    elif aggregation_method == "max_probs":
+        # Compute max probabilities per document
+        max_values = np.where(mask[:, :, None], log_probabilities[:, None, :], float('-inf'))
+        max_log_probs = np.max(max_values, axis=0)
+        aggregated_predictions = np.argmax(max_log_probs, axis=-1)
+        scores = np.exp(max_log_probs.max(axis=-1))
+
+    elif aggregation_method == "majority_vote":
+        # Convert logits to actual predictions before voting
+        predictions = np.argmax(logits, axis=1)
+        # Tally the votes for each document
+        vote_tally = np.zeros((num_docs, logits.shape[-1]), dtype=int)
+        np.add.at(vote_tally, overflow_to_sample_mapping, np.eye(logits.shape[-1], dtype=int)[predictions])
+        # Determine the majority vote for each document
+        aggregated_predictions = np.argmax(vote_tally, axis=1)
+        scores = vote_tally.max(axis=-1) / vote_tally.sum(axis=-1)
+    else:
+        raise ValueError(f"Unsupported aggregation_method: {aggregation_method}")
+
+
+    return aggregated_predictions, scores
+
+
+class CustomTrainer(Trainer):
+    def __init__(self, *args, loss_type="average_log_probs", **kwargs):
+        super(CustomTrainer, self).__init__(*args, **kwargs)
+        self.loss_type = loss_type
+        self.nll_loss = nn.NLLLoss()
+
+
+    def compute_loss(self, model, inputs, return_outputs=False):
+
+        labels = inputs["labels"]
+        overflow_to_sample_mapping = inputs.pop("overflow_to_sample_mapping", None)
+        doc_labels = inputs.pop("doc_labels", None)
+        logits = model(**inputs).logits
+
+        # print(logits)
+
+        # convert logits to log probabilities, probabilities
+        log_probabilities = F.log_softmax(logits, dim=-1)
+        probabilities = F.softmax(logits, dim=-1)
+
+
+
+        # Number of unique documents and chunks
+        num_docs = doc_labels.size(0)
+        num_chunks = overflow_to_sample_mapping.size(0)
+
+        # Ensure overflow_to_sample_mapping is on the same device as doc_labels
+        overflow_to_sample_mapping = overflow_to_sample_mapping.to(doc_labels.device)
+
+        # Create a tensor representing each unique doc
+        unique_docs = torch.arange(num_docs).to(doc_labels.device)
+
+        # Create the mask
+        mask = overflow_to_sample_mapping[:, None] == unique_docs[None, :]
+
+
+        if self.loss_type == "average_log_probs":
+            avg_log_probs = (log_probabilities[:, None, :] * mask.unsqueeze(-1).float()).sum(0) / mask.sum(0, keepdim=True).T
+
+            # print('avg_log_probs_shape', avg_log_probs.shape)
+            loss = self.nll_loss(avg_log_probs, doc_labels)
+            # print(loss)
+
+        elif self.loss_type == "average_probs":
+            avg_probs = (probabilities[:, None, :] * mask.unsqueeze(-1).float()).sum(0) / mask.sum(0, keepdim=True).T
+
+            avg_log_probs = torch.log(avg_probs)
+            # print('avg_log_probs_shape', avg_log_probs.shape)
+            loss = self.nll_loss(avg_log_probs, doc_labels)
+            # print(loss)
+
+        elif self.loss_type == "max":
+            max_values = torch.where(mask.unsqueeze(-1), log_probabilities[:, None, :], torch.tensor(float('-inf'), device=log_probabilities.device))
+            max_log_probs = max_values.max(dim=0).values
+            # print('max_log_probs_shape', max_log_probs.shape)
+            loss = self.nll_loss(max_log_probs, doc_labels)
+            # print(loss)
+
+        elif self.loss_type == "loss_per_chunk":
+            # print('log_probs_shape', log_probabilities.shape)
+            loss = self.nll_loss(log_probabilities, labels.view(-1))
+            # print(loss)
+
+        else:
+            raise ValueError(f"Unsupported loss_type: {self.loss_type}")
+
+
+        return (loss, logits) if return_outputs else loss
+    
+
+def evaluate_model(dataloader: DataLoader, model: PreTrainedModel, metric_only: bool = True, aggregation_method: str ='average_log_probs'):
+    # Load the accuracy metric
+    accuracy_metric = load("accuracy")
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    model = model.to(device=device)
+
+    predictions = []
+    all_doc_labels = []
+
+    # Loop over batches
+    for batch in dataloader:
+        model.eval()
+        doc_labels = batch.pop("doc_labels", None).cpu().numpy()
+        overflow_to_sample_mapping = batch.pop("overflow_to_sample_mapping", None).cpu().numpy()
+        batch = {k: v.to(device) for k, v in batch.items()}
+
+        with torch.no_grad():
+            outputs = model(**batch)
+            logits = outputs.logits.cpu().numpy()
+            aggregated_predictions, scores = aggregate_predictions(logits=logits,
+                                                            aggregation_method=aggregation_method,
+                                                            overflow_to_sample_mapping=overflow_to_sample_mapping)
+
+            accuracy_metric.add_batch(predictions=aggregated_predictions, references=doc_labels)
+            if not metric_only:
+                predictions.extend(aggregated_predictions)
+                all_doc_labels.extend(doc_labels)
+
+    # Calculate and return the final accuracy.
+    final_accuracy = accuracy_metric.compute()['accuracy']
+
+    if metric_only:
+        return final_accuracy
+    else:
+        return final_accuracy, predictions, all_doc_labels
+
+
+
+
